7d0107bfb5b473106d4584a9caa8e9946575cf79
Chak Tang <CHAKL@hotmail.com>
2021-12-08 16:04:31 +0100

    Finished 2.11: The Phonebook Step6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
@@ -6,17 +7,28 @@ import Persons from './components/Persons'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [showPersons, setShowPersons] = useState(persons)
   const [filterSearch, setFilterSearch] = useState('')
+
+  //[] means run one time after rendering.
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+    .then(response => setPersons(response.data))
+  }, [])
+
+    //Callback for hooks filterSearch, everytime filterSearch gets update run this.
+    useEffect(() => {
+      setShowPersons(persons.filter(person => person.name.toLowerCase().includes(filterSearch.toLowerCase())))
+    },[persons,filterSearch])
+  
+  
+    useEffect(() => {
+      setShowPersons(persons)
+    },[persons])
+
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -56,15 +68,6 @@ const App = () => {
     setFilterSearch(event.target.value)
   }
 
-  //Callback for hooks filterSearch, everytime filterSearch gets update run this.
-  useEffect(() => {
-    setShowPersons(persons.filter(person => person.name.toLowerCase().includes(filterSearch.toLowerCase())))
-  },[filterSearch])
-
-
-  useEffect(() => {
-    setShowPersons(persons)
-  },[persons])
 
   return (
     <div>
