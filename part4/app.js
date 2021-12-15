6125d48f159fcf5d20e3bad27c82d68089ee96bf
Chak Tang <CHAKL@hotmail.com>
2021-12-15 18:12:59 +0100

    Finished exercise 4.2, refacatored the app into separate modules , adhering to Node.js best practices.

---
+++ part4/app.js
@@ -0,0 +1,27 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const app = express()
+
+const cors = require('cors')
+
+const blogsRouter = require('./controllers/blogs')
+const mongoose = require('mongoose')
+
+app.use(cors())
+app.use(express.json())
+
+logger.info('connecting to', config.MONGO_URI)
+
+mongoose.connect(config.MONGO_URI)
+.then(() => {
+    logger.info('connected to MongoDB')
+})
+.catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+})
+
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
