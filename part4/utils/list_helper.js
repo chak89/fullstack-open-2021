e3604e84822b2e49894f7f5b8f7158585e6d3273
Chak Tang <CHAKL@hotmail.com>
2021-12-16 15:07:44 +0100

    Finished exercise 4.5* and 4.6*

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -4,23 +4,59 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
 
-  if(blogs.length === 0) {
+  if (blogs.length === 0) {
     return 0
   }
 
-  if(blogs.length === 1) {
+  if (blogs.length === 1) {
     return blogs[0].likes
   }
 
-  
+
   return blogs.reduce((previousValue, currentValue) => {
     const total = previousValue.likes + currentValue.likes
     //Returns a object with propery likes as the previousValue
-    return {likes: total}
+    return { likes: total }
   }).likes
+}
+
+
+const favoriteBlog = (blogs) => {
+  //Sort using Sort Compare Function 
+  //clone array so we don't change the original when using .sort()
+  //b - a sort in descending order
+  clonedBlogs = [...blogs]
+  clonedBlogs.sort((a, b) => {
+    return b.likes - a.likes
+  })
+
+  return clonedBlogs[0]
+}
+
+const mostBlogs = (blogs) => {
+  let authorsMap = new Map();
+
+  //Map each author and occurnce to a Map
+  blogs.forEach((blog) => {
+    if (authorsMap.has(blog.author)) {
+      authorsMap.set(blog.author, authorsMap.get(blog.author) + 1)
+    } else {
+      authorsMap.set(blog.author, 1)
+    }
+  })
+
+  //Convert from Map to an array of object, then sort in descending order by most blogs.
+  authorsArray = Array.from(authorsMap, ([author, blogs]) => ({ author, blogs }))
+  .sort((a,b) => {
+    return b.blogs - a.blogs
+  })
+
+  return authorsArray[0]
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog,
+  mostBlogs
 }
