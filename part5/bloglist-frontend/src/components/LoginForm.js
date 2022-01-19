d65eff8ef4145805836756c2452569c2c7fc5a0f
Chak Tang <CHAKL@hotmail.com>
2022-01-19 15:04:34 +0100

    Finished exercise 5.11

--- part5/bloglist-frontend/src/components/LoginForm.js
+++ part5/bloglist-frontend/src/components/LoginForm.js
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 const LoginForm = (props) => {
 	const { user, username, setUsername, password, setPassword, handleLogin } = props
 
@@ -28,6 +30,15 @@ const LoginForm = (props) => {
 			</div>
 		)
 	}
+}
+
+//Enforce mandatory props with PropTypes 
+LoginForm.propTypes = {
+	username: PropTypes.string.isRequired,
+	setUsername: PropTypes.func.isRequired,
+	password: PropTypes.string.isRequired,
+	setPassword: PropTypes.func.isRequired,
+	handleLogin: PropTypes.func.isRequired
 }
 
 export default LoginForm
