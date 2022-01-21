2429566d64492da46306f45886696d9d6c24066f
Chak Tang <CHAKL@hotmail.com>
2022-01-21 20:05:45 +0100

    Finished exercise 5.18

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,7 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
-    cy.visit('http://localhost:3000')
+		cy.request('POST', 'http://localhost:3003/api/users', {
+			username: 'testUser', password: 'testPassword', name: 'testName'
+		}).then(response => {
+			localStorage.setItem('loggedNoteappUser', JSON.stringify(response.body))
+			cy.visit('http://localhost:3000')
+		})
   })
 
   it('Login form is shown', function() {
@@ -9,6 +14,25 @@ describe('Blog app', function() {
 		cy.contains('login').click()
 		cy.get('#username').should('be.visible')
 		cy.get('#password').should('be.visible')
-  
+  })
+
+	describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+			cy.visit('http://localhost:3000')
+			cy.contains('login').click()
+			cy.get('#username').type('testUser')
+			cy.get('#password').type('testPassword')
+			cy.get('#login-button').click()
+			cy.contains('testName logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.visit('http://localhost:3000')
+			cy.contains('login').click()
+			cy.get('#username').type('WrongUserName')
+			cy.get('#password').type('WrongPassword')
+			cy.get('#login-button').click()
+			cy.contains('wrong username or password')
+    })
   })
 })
