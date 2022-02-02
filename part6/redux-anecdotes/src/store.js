7604916959ed54517764f9662231f90672fbdef6
Chak Tang <CHAKL@hotmail.com>
2022-02-02 10:30:16 +0100

    Finished 6.9 Better anecdotes, step7

---
+++ part6/redux-anecdotes/src/store.js
@@ -0,0 +1,14 @@
+import { createStore, combineReducers } from "redux";
+import anecdoteReducer from "./reducers/anecdoteReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({
+	anecdotes: anecdoteReducer,
+})
+
+const store = createStore(
+	reducer,
+	composeWithDevTools()
+)
+
+export default store;
