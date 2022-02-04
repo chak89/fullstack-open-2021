ee039a8f2cafb6eb46ae9d6a9f10c89f29bd6cc8
Chak Tang <CHAKL@hotmail.com>
2022-02-04 18:28:53 +0100

    Finished 6.14 Anecdotes and the backend, step2

--- part6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,16 +2,21 @@ import React from 'react'
 import { createAnecdote } from '../reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
 import { showNotification, removeNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
 
 	const dispatch = useDispatch()
 
-	const addAnecdote = (event) => {
+	const addAnecdote = async (event) => {
 		event.preventDefault()
 		const content = event.target.anecdote.value
 		event.target.anecdote.value = ''
-		dispatch(createAnecdote(content))
+
+
+		const newAnecdote = createAnecdote(content)
+		await anecdoteService.createNew(newAnecdote.data)
+		dispatch(newAnecdote)
 
 		dispatch(showNotification(`you created '${content}'`))
     setTimeout(() => dispatch(removeNotification()), 5000);
--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -9,13 +9,14 @@
 
 const getId = () => (100000 * Math.random()).toFixed(0)
 
+/* 
 const asObject = (anecdote) => {
 	return {
 		content: anecdote,
 		id: getId(),
 		votes: 0
 	}
-}
+} */
 
 export const createAnecdote = (content) => {
 	return {
