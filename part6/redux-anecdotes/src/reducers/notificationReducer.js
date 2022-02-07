d0bab402e7d651096e3a55118bbe820b76a29804
Chak Tang <CHAKL@hotmail.com>
2022-02-07 15:08:44 +0100

    Finished 6.21 anecdotes, the grand finale

--- part6/redux-anecdotes/src/reducers/notificationReducer.js
+++ part6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -1,11 +1,18 @@
 
+var timeouts = []
+
 export const showNotification = (content, timeInSeconds) => {
 	return async dispatch => {
+		clearTimeout()
 		dispatch({
 			type: 'SHOW_NOTIFICATION',
 			data: content
 		})
-		setTimeout(() => dispatch(removeNotification()), timeInSeconds*1000);
+
+		//Store timeouts in an array so we can cancel the previous established timeout.
+		//So only the latest notification is counted.
+		clearTimeout(timeouts[timeouts.length - 1])
+		timeouts.push(setTimeout(() => dispatch(removeNotification()), timeInSeconds*1000))
 	}
 }
 
