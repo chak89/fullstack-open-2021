980f70a6454c08fd830d3444eec850dd29d58542
Chak Tang <CHAKL@hotmail.com>
2022-02-08 19:55:46 +0100

    Refactored into components + finished 7.3: routed anecdotes, step3

---
+++ part7/routed-anecdotes/src/components/Anecdote.js
@@ -0,0 +1,18 @@
+import {
+	useParams
+} from "react-router-dom"
+
+const Anecdote = ({ anecdotes }) => {
+	const { id } = useParams();
+	const anecdote = anecdotes.find(a => a.id === id)
+
+	return (
+		<div>
+			<h2>{anecdote.content}</h2>
+			<p>has {anecdote.votes} votes</p>
+			<p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+		</div>
+	)
+}
+
+export default Anecdote
