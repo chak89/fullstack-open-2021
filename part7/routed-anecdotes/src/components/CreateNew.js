afa30567b5839a2c695a5fdb606a167063d50f7e
Chak Tang <CHAKL@hotmail.com>
2022-02-09 09:49:53 +0100

    Finished 7.5: anecdotes and hooks step2

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -19,6 +19,12 @@ const CreateNew = (props) => {
 		history.push('/')
 	}
 
+	const resetForm = () =>{
+		content.reset()
+		author.reset()
+		info.reset()
+	}
+
 	return (
 		<div>
 			<h2>create a new anecdote</h2>
@@ -35,7 +41,8 @@ const CreateNew = (props) => {
 					url for more info
 					<input name='info' {...info} />
 				</div>
-				<button>create</button>
+				<button type='submit'>create</button>
+				<button type='button' onClick={resetForm}>reset</button>
 			</form>
 		</div>
 	)
