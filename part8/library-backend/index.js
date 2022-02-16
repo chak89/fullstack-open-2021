1210cefe198e394ebdc23d642383e5f1edf51dce
Chak Tang <CHAKL@hotmail.com>
2022-02-16 12:16:13 +0100

    Finished 8.2: All books
    Implement query allBooks, which returns the details of all books.

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -89,16 +89,26 @@ let books = [
 ]
 
 const typeDefs = gql`
+	type books {
+		title: String!
+		published: Int!
+		author: String!
+		id: String!
+		genres: [String]!
+	}
+
   type Query {
 		bookCount: Int!
 		authorCount: Int!
+		allBooks: [books!]!
   }
 `
 
 const resolvers = {
   Query: {
 		bookCount: () => books.length,
-		authorCount: () => authors.length
+		authorCount: () => authors.length,
+		allBooks: () => books
   }
 }
 
