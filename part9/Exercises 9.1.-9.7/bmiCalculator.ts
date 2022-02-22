4f5fb3f8c8aa8f414559d1d0901ada280973f53b
Chak Tang <CHAKL@hotmail.com>
2022-02-22 13:36:42 +0100

    Finished 9.5 WebBMI
    Add an endpoint for the BMI calculator that can be used by doing an HTTP GET request to endpoint bmi and specifying the input with query string parameters.

--- part9/Exercises 9.1.-9.7/bmiCalculator.ts
+++ part9/Exercises 9.1.-9.7/bmiCalculator.ts
@@ -9,12 +9,15 @@ const calculateBmi = (height: number, weight: number): String => {
 		case (bmi >=18.5 && bmi <= 24.9):
 			return 'Normal (healthy weight)';
 		case (bmi > 24.9):
+			return 'Underweight';
 			break;
 		default:
-			return 'Overweight';
+			return 'Out of range';
 	}
 }
-
+/* 
 const height: number = Number(process.argv[2]);
 const weight: number = Number(process.argv[3]);
-console.log(calculateBmi(height, weight));
+console.log(calculateBmi(height, weight)); */
+
+export default calculateBmi;
--- part9/Exercises 9.1.-9.7/index.ts
+++ part9/Exercises 9.1.-9.7/index.ts
@@ -1,12 +1,36 @@
 import express from 'express';
+import calculateBmi from './bmiCalculator';
+
 const app = express();
 
 app.get('/hello', (_req, res) => {
-  res.send('Hello Full Stack!');
+	res.send('Hello Full Stack!');
 });
 
-const PORT = 3003;
+
+app.get('/bmi', (req, res) => {
+
+	if (!req.query) {
+		return res.status(400).send({ error: 'malformatted parameters' })
+	}
+
+	if (!req.query.height || !req.query.weight) {
+		return res.status(400).send({ error: 'malformatted parameters' })
+	}
+
+	const height = Number(req.query.height);
+	const weight = Number(req.query.weight);
+
+	const result = {
+		...req.query,
+		bmi: calculateBmi(height, weight)
+	}
+
+	return res.send(result);
+});
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
