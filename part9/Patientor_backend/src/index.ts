2c2bb8167fb9dd1f24a4d720ffa7f0f9b98cb09a
Chak Tang <CHAKL@hotmail.com>
2022-02-23 17:04:40 +0100

    Finished 9.10: Patientor backend, step3
    Structured code properly. Created endpoint /api/diagnoses for fetching all diagnoses with HTTP GET.

--- part9/Patientor_backend/src/index.ts
+++ part9/Patientor_backend/src/index.ts
@@ -1,14 +1,16 @@
 import express from 'express';
+import diagnosesRouter from './routes/diagnoses';
 
 const app = express();
 app.use(express.json());
-
 
 
 app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
 
 const PORT = 3001;
 app.listen(PORT, () => {
